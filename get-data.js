7e99ee6ea5a3f0d5dc5b6108372e93f3d8263f6b
Alexander Rivaldy <alexanderrivaldy@gmail.com>
2026-09-06 22:12:50 +1000

    data.js

--- get-data.js
+++ get-data.js
@@ -1,2 +1,7 @@
 
-fetch()
+const baseData = await fetch("https://raw.githubusercontent.com/alexander-rivaldy/stolen-treasure-assets/refs/heads/main/dragons-lair/base/data.json")
+
+for (let idx = 0; idx < array.length; idx++) {
+    const element = baseData[idx];
+    console.log(element)
+}
